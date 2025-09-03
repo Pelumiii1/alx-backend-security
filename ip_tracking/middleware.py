82882971093f8b5c38f91825548de3083af6cc9a
Pelumiii1 <adesokanpelumi14@gmail.com>
2025-09-03 21:34:20 +0100

    IP Blacklisting

--- ip_tracking/middleware.py
+++ ip_tracking/middleware.py
@@ -1,4 +1,5 @@
-from .models import RequestLog
+from .models import RequestLog, BlockedIP
+from django.http import HttpResponseForbidden
 
 class IPTrackingMiddleware:
     def __init__(self, get_response):
@@ -6,6 +7,10 @@ class IPTrackingMiddleware:
 
     def __call__(self, request):
         ip_address = request.META.get('REMOTE_ADDR')
+
+        if BlockedIP.objects.filter(ip_address=ip_address).exists():
+            return HttpResponseForbidden('<h1>Forbidden</h1><p>Your IP address has been blocked.</p>')
+
         path = request.path
         RequestLog.objects.create(ip_address=ip_address, path=path)
         response = self.get_response(request)
